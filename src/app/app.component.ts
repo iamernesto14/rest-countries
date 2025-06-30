c963fc25ef07823fb0ef1a5e07659a4afd09829f
iamernesto14 <eernesto211@gmail.com>
2025-06-30 16:17:33 +0000

    Feat: Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./components/home/home.component";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
